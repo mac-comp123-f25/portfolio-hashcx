4e2f37133e865f961d2178a60c80092a1f746888
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2025-09-08 08:22:11 -0500

    Update greet.py

--- src/ica/02-data_types/greet.py
+++ src/ica/02-data_types/greet.py
@@ -1,2 +1,6 @@
 kind_person = 'Anut Judi'
-
+thank_you_msg = 'Dear ' + kind_person + """
+Thank you for the graduation present. I can’t wait to use it. I
+hope you are enjoying your summer. You’re the best, """ + kind_person + """!
+Love, Mittens"""
+print(thank_you_msg)
