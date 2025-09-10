13c873d1fd3b8dc8c1b8608764d19f706a432559
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2025-09-10 09:16:01 -0500

    Create star.py

---
+++ src/ica/03-turtle/star.py
@@ -0,0 +1,47 @@
+import turtle
+
+screen = turtle.Screen()
+
+tur = turtle.Turtle()
+tur.color("red")
+tur.shape("turtle")
+
+tur.forward(100)
+tur.left(180-36)
+
+tur.forward(100)
+tur.left(180-36)
+
+tur.forward(100)
+tur.left(180-36)
+
+tur.forward(100)
+tur.left(180-36)
+
+tur.forward(100)
+tur.left(180-36)
+
+tur2 = turtle.Turtle()
+tur2.color("blue")
+tur2.shape("turtle")
+tur2.penup()
+tur2.goto(0,-100)
+tur2.pendown()
+
+tur2.forward(100)
+tur2.left(180-36)
+
+tur2.forward(100)
+tur2.left(180-36)
+
+tur2.forward(100)
+tur2.left(180-36)
+
+tur2.forward(100)
+tur2.left(180-36)
+
+tur2.forward(100)
+tur2.left(180-36)
+
+
+screen.exitonclick()
