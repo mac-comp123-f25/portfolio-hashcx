91a3cf98781567c93d5a2881f6deb60f366ad432
Amin G. Alhashim, PhD, 6σ <amin.g.alhashim@gmail.com>
2025-10-20 10:37:43 -0500

    Create pyinstall.py

---
+++ pyinstall.py
@@ -0,0 +1,7 @@
+# reference: https://www.youtube.com/watch?v=JZzu1mLXkog
+
+import PyInstaller.__main__
+
+PyInstaller.__main__.run(['src/welcome/main.py', '--windowed', '--noconsole', '--icon=assets/pumpkin.ico'])
+
+
