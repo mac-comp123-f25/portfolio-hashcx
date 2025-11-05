f1b91be761fa7cd3b604de68d5eac90d1cfcc1ff
Amin Alhashim <aalhashi@macalester.edu>
2025-11-05 11:12:06 -0600

    Update simple_example.py

--- src/sandbox/modern-tkinter/simple_example.py
+++ src/sandbox/modern-tkinter/simple_example.py
@@ -8,8 +8,11 @@ customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-bl
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
 app.geometry("400x240")
 
+
 def button_function():
+    button.configure(text="???")
     print("button pressed")
+
 
 # Use CTkButton instead of tkinter Button
 button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
